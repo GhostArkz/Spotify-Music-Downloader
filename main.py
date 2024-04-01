deec7c308829cbe1a1920d198a3d0e010058e38d
Sunshine Maxwell Jennings <smjennin@uci.edu>
2024-03-31 17:05:08 -0700

    Searches and downloads properly, currently set to retrieve first 5 songs only for testing purposes.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import os
 import spotipy
-import pytube
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 from pytube import YouTube
 from pytube import Search
 
-#SAVE_PATH = "C:\Users\Sonny\Downloads"
+SAVE_PATH = r"C:\Users\Sonny\Downloads\Spotify_Downloads"
 CLIENT_ID = "42f4851156894fd6b25c9118816ad740"
 CLIENT_SECRET = "14f28b59723c4eebab2e8fd28cf28533"
 REDIRECT_URI = "http://localhost:8888/callback"
@@ -16,34 +16,47 @@ spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope='user-library-read'))
 
 
-def get_liked_tracks():
+def download_audio(yt_url, destination='.'):
+    try:
+        yt = YouTube(yt_url)
+        video = yt.streams.filter(only_audio=True).first()
+        if video:
+            out_file = video.download(output_path=destination)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+            print(f"{yt.title} has been successfully downloaded.")
+        else:
+            print(f"No audio streams found for {yt.title}.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def get_liked_tracks(limit=5):
     liked_tracks = []
-    results = spotify.current_user_saved_tracks()
-    while results:
-        liked_tracks.extend([item['track'] for item in results['items']])
-        if results['next']:
+    results = spotify.current_user_saved_tracks(limit=limit)
+    while results and len(liked_tracks) < limit:
+        liked_tracks.extend(item['track'] for item in results['items'])
+        if results['next'] and len(liked_tracks) < limit:
             results = spotify.next(results)
         else:
             break
-    return liked_tracks
+    return liked_tracks[:limit]  # Ensure that only the top 5 tracks are returned
 
 
-#def search_liked_tracks():
-
-
-
-
-
-
+liked_tracks_url = []
 tracks = get_liked_tracks()
+
 for current_track in tracks:
-    s = Search(current_track['name'])
+    # ... Your existing code to get the YouTube URL for each track ...
+    s = Search(current_track['name'] + '-' + current_track['artists'][0]['name'] + "audio")
     v = s.results[0]
+    liked_tracks_url.append(v.watch_url)
     print(f"{v.title}\n{v.watch_url}\n")
 
-    """try:
-        tracks = get_liked_tracks()
-        for track in tracks:
-            print(track['name'], '-', track['artists'][0]['name'])
-    except Exception as e:
-        print(f"An error occurred: {e}")"""
+# Use the download_audio function for each URL
+for url in liked_tracks_url:
+    download_audio(url, SAVE_PATH)
+
+
+
